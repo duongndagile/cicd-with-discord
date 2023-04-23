d199a2ff2477c0896ccc995b2eb32c4ab27ec370
duongnd <duong.nd@agiletechsoftware.com>
2023-04-23 13:04:22 +0700

    update text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Demo ReactJs CI/CD with Discord</h1>
+      <h1>ReactJs CI/CD with Discord</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
